d991b6ddbf0fbb049d1cff05fefa1c0b6c64881d
Ryan Cross <sc13rjc@leeds.ac.uk>
2017-01-10 18:47:21 +0000

    Updated wallpaper and resolution script.

--- code/vmware/.res.sh
+++ code/vmware/.res.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 xrandr --newmode "3840x2160_60.00"  712.34  3840 4152 4576 5312  2160 2161 2164 2235  -HSync +Vsync
-xrandr --addmode Virtual-1 3840x2160_60.00
-xrandr --output Virtual-1 --mode 3840x2160_60.00
-feh --bg-scale /home/Downloads/space-shuttle-16092-1920x1080.jpg
+xrandr --addmode Virtual1 3840x2160_60.00
+xrandr --output Virtual1 --mode 3840x2160_60.00
+feh --bg-scale /home/ryanc/Downloads/background.png
