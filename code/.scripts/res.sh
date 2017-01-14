133c72bccfa8646b6415890703fdfa5084ae5b5c
Ryan Cross <sc13rjc@leeds.ac.uk>
2017-01-14 18:16:26 +0000

    Swapped Rofi to be transparent, fixed resolution script.

--- code/.scripts/res.sh
+++ code/.scripts/res.sh
@@ -3,4 +3,4 @@
 xrandr --newmode "3840x2160_60.00"  712.34  3840 4152 4576 5312  2160 2161 2164 2235  -HSync +Vsync
 xrandr --addmode Virtual1 3840x2160_60.00
 xrandr --output Virtual1 --mode 3840x2160_60.00
-feh --bg-scale /home/ryanc/Downloads/background.png
+feh --bg-scale /usr/share/backgrounds/background.png
