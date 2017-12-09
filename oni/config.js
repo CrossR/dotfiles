eca93fd3f8ae6f88c801b7f5cb6b436b896e9ab3
CrossR <sc13rjc@leeds.ac.uk>
2017-12-09 01:20:50 +0000

    Update Oni Activate to include zoom.

--- oni/config.js
+++ oni/config.js
@@ -1,6 +1,12 @@
 const activate = (oni) => {
-    // access the Oni plugin API here
-    oni.input.bind("<c-enter>", () => oni.recorder.takeScreenshot());
+    // Take a screenshot on Control+Enter is pressed
+    oni.input.bind("<c-enter>", () => oni.recorder.takeScreenshot())
+
+    // Set zoom factor to 1.5 when Control+= is pressed
+    oni.input.bind("<c-=>", () => require("electron").remote.getCurrentWindow().webContents.setZoomFactor(1.5))
+
+    // Set zoom factor to 1 when Control+- is pressed
+    oni.input.bind("<c-->", () => require("electron").remote.getCurrentWindow().webContents.setZoomFactor(1))
 };
 
 module.exports = {
