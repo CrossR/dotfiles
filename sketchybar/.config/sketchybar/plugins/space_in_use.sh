e1fac2103458b32da84b2bab0804fac5120e29ac
CrossR <r.cross@lancaster.ac.uk>
2021-11-19 11:32:03 +0000

    Show the number of windows open in each space.

--- sketchybar/.config/sketchybar/plugins/space_in_use.sh
+++ sketchybar/.config/sketchybar/plugins/space_in_use.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
+COUNTERS=(¹ ² ³ ⁴ ⁵ ⁶ ⁷ ⁸ ⁹)
+
 args=()
-while read -r index window
-do
-  if [ "$window" = "null" ]
-  then
+while read -r index windows; do
+  if [ "$windows" -eq 0 ]; then
     args+=(--set "space${index}" "icon=${index}")
   else
-    args+=(--set "space${index}" "icon=${index}°")
+    i=$((windows-1))
+    args+=(--set "space${index}" "icon=${index}${COUNTERS[$i]}")
   fi
-done <<< "$(yabai -m query --spaces | jq -r '.[] | [.index, .windows[0]] | @sh')"
+done <<< "$(yabai -m query --spaces | jq -r '.[] | [.index, .windows | length] | @sh')"
 
 sketchybar -m "${args[@]}"
 
