679caaf86d09bb718dd0c16a944fb7f50c07c816
CrossR <r.cross@lancaster.ac.uk>
2021-11-19 00:38:19 +0000

    Update the space indicator for sketchybar.
    Only show available spaces, hide missing ones.
    Add an indicator to show if a space has windows.

---
+++ sketchybar/.config/sketchybar/plugins/space_in_use.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+args=()
+while read -r index window
+do
+  if [ "$window" = "null" ]
+  then
+    args+=(--set "space${index}" "icon=${index}")
+  else
+    args+=(--set "space${index}" "icon=${index}°")
+  fi
+done <<< "$(yabai -m query --spaces | jq -r '.[] | [.index, .windows[0]] | @sh')"
+
+sketchybar -m "${args[@]}"
+
