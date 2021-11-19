43360d36d77b643ad9fa91507d7296fc12b3d984
CrossR <r.cross@lancaster.ac.uk>
2021-11-19 15:30:28 +0000

    Drop debug prints.

--- sketchybar/.config/sketchybar/plugins/space_active.sh
+++ sketchybar/.config/sketchybar/plugins/space_active.sh
@@ -2,17 +2,14 @@
 
 ACTIVE_SPACES=$(yabai -m query --spaces | jq -r '.[] | [.index] | @sh')
 
-echo $ACTIVE_SPACES
-
 for i in {1..10}; do
-    echo "Checking $i"
+
     if [[ "${ACTIVE_SPACES[*]}" =~ "${i}" ]]; then
-        echo "$i active!"
         sketchybar -m --set space${i} icon.drawing=on
     else
-        echo "$i not active!"
         sketchybar -m --set space${i} icon.drawing=off
     fi
+
 done
 
 
--- sketchybar/.config/sketchybar/plugins/space_in_use.sh
+++ sketchybar/.config/sketchybar/plugins/space_in_use.sh
@@ -4,12 +4,21 @@ COUNTERS=(¹ ² ³ ⁴ ⁵ ⁶ ⁷ ⁸ ⁹)
 
 args=()
 while read -r index windows; do
-  if [ "$windows" -eq 0 ]; then
-    args+=(--set "space${index}" "icon=${index}")
-  else
-    i=$((windows-1))
-    args+=(--set "space${index}" "icon=${index}${COUNTERS[$i]}")
-  fi
+    if [ "$windows" -eq 0 ]; then
+        args+=(--set "space${index}" "icon=${index}")
+    else
+        i=$((windows-1))
+        args+=(--set "space${index}" "icon=${index}${COUNTERS[$i]}")
+    fi
+
+    # if [[ ${is_full} == "true" ]]; then
+    #     sketchybar -m --set space${space} icon.font="Hack Nerd Font:Bold:16.0"
+    #     sketchybar -m --set space${space} icon=
+    # else
+    #     sketchybar -m --set space${space} icon.font='SF Pro:Semibold:14.0'
+    #     sketchybar -m --set space${space} icon=${space}
+    # fi
+
 done <<< "$(yabai -m query --spaces | jq -r '.[] | [.index, .windows | length] | @sh')"
 
 sketchybar -m "${args[@]}"
