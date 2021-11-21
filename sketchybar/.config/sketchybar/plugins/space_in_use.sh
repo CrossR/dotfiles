a16d231a58462056158cf8cfb22104e259609295
CrossR <r.cross@lancaster.ac.uk>
2021-11-21 00:04:56 +0000

    Ignore mutli-workspace apps from Sketchybar counter.

--- sketchybar/.config/sketchybar/plugins/space_in_use.sh
+++ sketchybar/.config/sketchybar/plugins/space_in_use.sh
@@ -2,24 +2,41 @@
 # Set the space icons to:
 #  - Number of the space
 #  - Number with superscript number of windows
-#  - A fullscreen logo for native fullscreen apps
+#  - A fullscreen logo for native full screen apps
+#
+# Queries the actual windows in each space to filter the results.
 
 COUNTERS=(¹ ² ³ ⁴ ⁵ ⁶ ⁷ ⁸ ⁹)
 
 args=()
 while read -r index is_full windows; do
 
-    if [ "$windows" -eq 0 ]; then
+    if [ "$windows" -eq 0 ] && [ "${is_full}" == false ]; then
         args+=(--set "space${index}" "icon=${index}")
     fi
 
+    filtered_window_num=0
+
     if [ "$windows" -gt 0 ] && [ "${is_full}" == "false" ]; then
-        i=$((windows-1))
+        # Ignore certain windows (mostly ones that persist over every space).
+        filtered_window_num=$(
+            yabai -m query --windows --space ${index} | \
+                jq -r '[.[] | select(
+                    .app != "Slack" and
+                    .app != "Discord" and
+                    .app != "Spotify"
+                )] | length | @sh'
+        )
+    fi
+
+    if [ "${filtered_window_num}" -gt 0 ]; then
+
+        i=$((filtered_window_num - 1))
         args+=(--set "space${index}" "icon=${index}${COUNTERS[$i]}")
     fi
 
     if [[ ${is_full} == "true" ]]; then
-        sketchybar -m --set "space${index}" icon.font="Hack Nerd Font:Bold:16.0"
+        sketchybar -m --set "space${index}" icon.font="Hack Nerd Font:Bold:20.0"
         sketchybar -m --set "space${index}" icon=
     else
         sketchybar -m --set "space${index}" icon.font='SF Pro:Semibold:14.0'
