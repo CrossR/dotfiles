4ec4274c1d696f294c1a4c46915ebb5bb4694af1
CrossR <r.cross@lancaster.ac.uk>
2021-12-14 16:05:42 +0000

    Tidy up how dropbox is detected for Sketchybar.

--- sketchybar/.config/sketchybar/plugins/app_icons.sh
+++ sketchybar/.config/sketchybar/plugins/app_icons.sh
@@ -4,7 +4,7 @@
 # Filter is used to ignore any other matching processes from that app.
 
 PROCESSES=(
-    "dropbox"
+    "/Applications/Dropbox.app/Contents/MacOS/Dropbox"
     "slack"
     "discord"
 )
@@ -15,27 +15,16 @@ ICONS=(
     "ﭮ"
 )
 
-FILTERS=(
-    "garcon"
-    ""
-    ""
-)
-
 ICON_STRING=""
 
 for (( i=0; i<${#PROCESSES[@]}; i++ )); do
 
     process=${PROCESSES[$i]}
-    filter=${FILTERS[$i]}
     icon=${ICONS[$i]}
 
-    if [ "${filter}" != "" ]; then
-        process_num=$(ps aux | grep -v grep | grep -vi "${filter}" | grep -ci "${process}")
-    else
-        process_num=$(ps aux | grep -v grep | grep -ci "${process}")
-    fi
+    process_num=$(ps aux | grep -v grep | grep -ci "${process}")
 
-    echo "$process, $filter, $process_num"
+    echo "$process, $process_num"
 
     if [ "${process_num}" -gt 0 ]; then
         ICON_STRING="${icon}  ${ICON_STRING}"
