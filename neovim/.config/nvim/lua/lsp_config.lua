935c5c47494ae6f372dc72cb4b6ff5372de90396
CrossR <r.cross@lancaster.ac.uk>
2023-01-05 11:41:23 +0000

    Tidy up nvim config, fix deprecation warning.

--- neovim/.config/nvim/lua/lsp_config.lua
+++ neovim/.config/nvim/lua/lsp_config.lua
@@ -140,7 +140,7 @@ cmp.setup.cmdline('/', {
 -- Finally, combine
 -------------------------------------------------------------------------------
 
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 local servers = { 'pyright', 'clangd' }
 
 for _, lsp in ipairs(servers) do
