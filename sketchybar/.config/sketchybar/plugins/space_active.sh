a4e0eccd14f55602495120f0ce435e7de4a44248
CrossR <r.cross@lancaster.ac.uk>
2023-01-05 11:41:55 +0000

    Refactor sketchybar spaces to deal with multimonitor.

--- sketchybar/.config/sketchybar/plugins/space_active.sh
+++ sketchybar/.config/sketchybar/plugins/space_active.sh
@@ -5,9 +5,9 @@ ACTIVE_SPACES=$(yabai -m query --spaces | jq -r '.[] | [.index] | @sh')
 for i in {1..10}; do
 
     if [[ "${ACTIVE_SPACES[*]}" =~ "${i}" ]]; then
-        sketchybar -m --set "space${i}" icon.drawing=on
+        sketchybar -m --set "space.${i}" icon.drawing=on
     else
-        sketchybar -m --set "space${i}" icon.drawing=off
+        sketchybar -m --set "space.${i}" icon.drawing=off
     fi
 
 done
--- sketchybar/.config/sketchybar/plugins/space_in_use.sh
+++ sketchybar/.config/sketchybar/plugins/space_in_use.sh
@@ -27,18 +27,18 @@ while read -r index is_full windows; do
 
     if [ "${filtered_window_num}" -gt 0 ]; then
         i=$((filtered_window_num - 1))
-        args+=(--set "space${index}" "icon=${index}${COUNTERS[$i]}")
+        args+=(--set "space.${index}" "icon=${index}${COUNTERS[$i]}")
     fi
 
     if [ "$filtered_window_num" -eq 0 ] && [ "${is_full}" == false ]; then
-        args+=(--set "space${index}" "icon=${index}")
+        args+=(--set "space.${index}" "icon=${index}")
     fi
 
     if [[ ${is_full} == "true" ]]; then
-        sketchybar -m --set "space${index}" icon.font="Hack Nerd Font:Bold:20.0"
-        sketchybar -m --set "space${index}" icon=
+        sketchybar -m --set "space.${index}" icon.font="Hack Nerd Font:Bold:20.0"
+        sketchybar -m --set "space.${index}" icon=
     else
-        sketchybar -m --set "space${index}" icon.font='SF Pro:Semibold:14.0'
+        sketchybar -m --set "space.${index}" icon.font='SF Pro:Semibold:14.0'
     fi
 
 done <<< "$(yabai -m query --spaces | jq -r '.[] | [.index, ."is-native-fullscreen", (.windows | length)] | @sh')"
