1ba4852a3a063c34dcc3f46da8c6f6bccd8dd2ba
CrossR <r.cross@lancaster.ac.uk>
2023-08-30 14:17:50 +0100

    Swap all app icons to use Slack script.
    1 bar icon per app, coloured based on the notification status.

---
+++ sketchybar/.config/sketchybar/plugins/app_icons_notif.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+declare -A ICONS=(
+    ["Slack"]="󰒱"
+    ["Dropbox"]=""
+    ["Discord"]="ﭮ"
+    ["Teams"]="󰊻"
+    ["Microsoft Outlook"]="󰴢"
+    ["Mattermost"]="󱧍"
+)
+ICON="${ICONS["$NAME"]}"
+
+STATUS_LABEL=$(lsappinfo info -only StatusLabel "$NAME")
+LABEL=""
+ICON_COLOR=""
+
+if [[ $STATUS_LABEL =~ \"label\"=\"([^\"]*)\" ]]; then
+    LABEL="${BASH_REMATCH[1]}"
+
+    if [[ $LABEL == "" ]]; then
+        ICON_COLOR="0xFFA6DA95"
+    elif [[ $LABEL == "•" ]]; then
+        ICON_COLOR="0xFFEED49F"
+    elif [[ $LABEL =~ ^[0-9]+$ ]]; then
+        ICON_COLOR="0xFFED8796"
+    else
+        ICON_COLOR="0xF0A8A8A8"
+    fi
+fi
+
+if [ "${ICON_COLOR}" != "" ]; then
+    sketchybar -m --set "$NAME" icon.drawing=on icon="${ICON}" label="${LABEL}" icon.color="${ICON_COLOR}"
+else
+    sketchybar -m --set "$NAME" drawing=off
+fi
