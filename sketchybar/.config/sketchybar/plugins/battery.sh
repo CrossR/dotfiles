67bc7c16150b0adec502cb81fa1b9b708330222e
CrossR <ryan.cross@warwick.ac.uk>
2024-02-12 16:24:32 +0000

    Fix battery icons.

--- sketchybar/.config/sketchybar/plugins/battery.sh
+++ sketchybar/.config/sketchybar/plugins/battery.sh
@@ -14,23 +14,23 @@ esac
 if [[ $CHARGING != "" ]]; then
   sketchybar -m --set battery \
     icon.color=${COLOR} \
-    icon= \
+    icon=󰂄\
     label=$(printf "${BATT_PERCENT}%%")
   exit 0
 fi
 
 case ${BATT_PERCENT} in
-    100) ICON="" ;;
-    9[0-9]) ICON="" ;;
-    8[0-9]) ICON="" ;;
-    7[0-9]) ICON="" ;;
-    6[0-9]) ICON="" ;;
-    5[0-9]) ICON="" ;;
-    4[0-9]) ICON="" ;;
-    3[0-9]) ICON="" ;;
-    2[0-9]) ICON="" ;;
-    1[0-9]) ICON="" ;;
-    *) ICON=""
+    100) ICON="󰁹" ;;
+    9[0-9]) ICON="󰂂" ;;
+    8[0-9]) ICON="󰂁" ;;
+    7[0-9]) ICON="󰂀" ;;
+    6[0-9]) ICON="󰁿" ;;
+    5[0-9]) ICON="󰁾" ;;
+    4[0-9]) ICON="󰁽" ;;
+    3[0-9]) ICON="󰁼" ;;
+    2[0-9]) ICON="󰁻" ;;
+    1[0-9]) ICON="󰁺" ;;
+    *) ICON="󰂎"
 esac
 
 sketchybar -m --set battery \
