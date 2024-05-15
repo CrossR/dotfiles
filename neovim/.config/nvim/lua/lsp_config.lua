2a8e2f16ff5f1b52c7cd57a643231b4f64911e2d
CrossR <ryan.cross@warwick.ac.uk>
2024-05-15 14:54:53 +0100

    Tidy up kitty conf.

--- neovim/.config/nvim/lua/lsp_config.lua
+++ neovim/.config/nvim/lua/lsp_config.lua
@@ -70,7 +70,7 @@ end
 local has_words_before = function()
     unpack = unpack or table.unpack
     local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-    return col ~= 0 and vim.api.nvim.buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
 
 cmp.setup({
