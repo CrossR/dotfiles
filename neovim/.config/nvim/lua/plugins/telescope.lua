2c369b840174b2f6cd7ff46722fe14eb7a33d91c
CrossR <ryan.cross@warwick.ac.uk>
2024-06-11 15:16:54 +0100

    Test out FzfLua.

--- neovim/.config/nvim/lua/plugins/telescope.lua
+++ neovim/.config/nvim/lua/plugins/telescope.lua
@@ -154,10 +154,40 @@ return {
         setup = function()
             require("telescope").load_extension("fzf")
         end,
-        keys = {
-            { "<c-p>", "<CMD>lua project_files()<CR>" },
-            { "<c-f>", "<CMD>Telescope live_grep<CR>" },
-            { "<leader>b", "<CMD>Telescope buffers<CR>" },
-        },
     },
+    {
+        "ibhagwan/fzf-lua",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        config = function()
+            local actions = require("fzf-lua.actions")
+
+            local function file_edit_or_tab(selected, opts)
+                if #selected > 1 then
+                    return actions.file_tabedit(selected, opts)
+                else
+                    return actions.file_edit(selected, opts)
+                end
+            end
+
+            require"fzf-lua".setup({
+                "telescope",
+                winopts = { preview = {default = "bat" } },
+                files = { path_shorten=1 },
+                actions = {
+                    files = {
+                        ["default"] = file_edit_or_tab,
+                        ["ctrl-s"] = actions.file_split,
+                        ["ctrl-v"] = actions.file_vsplit,
+                        ["ctrl-t"] = actions.file_tabedit,
+                        ["alt-q"] = actions.file_sel_to_qf,
+                    }
+                }
+            })
+        end,
+        keys = {
+            { "<c-p>", "<CMD>FzfLua files<CR>" },
+            { "<c-f>", "<CMD>FzfLua grep_project<CR>" },
+            { "<leader>b", "<CMD>FzfLua buffers<CR>" },
+        },
+    }
 }
--- neovim/.config/nvim/lua/plugins/ui.lua
+++ neovim/.config/nvim/lua/plugins/ui.lua
@@ -50,17 +50,32 @@ return {
     },
     {
         "nvim-lualine/lualine.nvim",
+        dependencies = { 'AndreM222/copilot-lualine' },
         config = function()
             require("lualine").setup({
                 options = {
                     theme = "gruvbox_dark",
+                },
+                sections =  {
+                    lualine_a = { 'mode' },
+                    lualine_b = { 'branch', 'diff',
+                        {
+                            'diagnostics',
+                            sources = { "nvim_diagnostic" },
+                            symbols = { error = ' ', warn = ' ', info = ' ', hint = ' ' }
+                        }
+                    },
+                    lualine_c = { 'filename' },
+                    lualine_x = { 'copilot' ,'encoding', 'fileformat', 'filetype' },
+                    lualine_y = { 'progress' },
+                    lualine_z = { 'location' }
                 },
             })
         end,
     },
     {
         "lewis6991/gitsigns.nvim",
-        requires = { "nvim-lua/plenary.nvim" },
+        dependencies = { "nvim-lua/plenary.nvim" },
         config = function()
             require("gitsigns").setup({
                 signs = {},
