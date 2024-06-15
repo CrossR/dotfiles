42301cd3aea5fb920122e419964bad4ee5d03a85
CrossR <ryan.cross@warwick.ac.uk>
2024-06-15 21:00:47 +0100

    Fix up headphones icon.

--- sketchybar/.config/sketchybar/plugins/headphones.sh
+++ sketchybar/.config/sketchybar/plugins/headphones.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DEVICES="$(system_profiler SPBluetoothDataType -json -detailLevel basic 2>/dev/null | jq '.SPBluetoothDataType' | jq '.[0]' | jq '.devices_list' | jq -r '.[] | keys[] as $k | "\($k) \(.[$k] | .device_connected) \(.[$k] | .device_minorType)"' | grep 'Yes' | grep 'Headset')"
+DEVICES="$(system_profiler SPBluetoothDataType -json -detailLevel basic 2>/dev/null | jq '.SPBluetoothDataType | .[0] | ."device_connected" | .[] | first(.[]) | .device_minorType')"
 
 if [ "$DEVICES" = "" ]; then
   sketchybar -m --set headphones drawing=off
