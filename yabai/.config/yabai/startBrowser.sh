ac7f36e6cb611e8d615f75dd1601336c13cb3006
CrossR <ryan.cross@warwick.ac.uk>
2024-06-15 17:58:30 +0100

    Remove unused yabai features.

--- yabai/.config/yabai/startBrowser.sh
+++ yabai/.config/yabai/startBrowser.sh
@@ -4,38 +4,6 @@
 OPTIND=1 # Reset so getopts works!
 SUCCESS=0
 
-PRIVATE_MODE=0
-
-show_help () {
-
-    cat << EOF
-USAGE
-    startBrowser.sh
-
-    Launch Firefox or Safari.
-    Firefox on AC Power, Safari otherwise.
-
-FLAGS
-    -p PrivateMode  [optional] Start browser in private mode.
-
-DEFAULTS
-
-EOF
-}
-
-while getopts "h?p" opt;
-do
-    case $opt in
-        h|\?)
-            show_help
-            return ${SUCCESS} 2> /dev/null || exit ${SUCCESS}
-            ;;
-        p)
-            PRIVATE_MODE=1
-            ;;
-    esac
-done
-
 BROWSER="firefox"
 PRIVATE="p"
 
@@ -43,14 +11,10 @@ ACTIVATE_BROWSER="tell application \"${BROWSER}\" to activate"
 ACTIVATE_KEYS="tell application \"System Events\" to keystroke "
 
 # Now the browser is running, interact with it via osascript.
-if [ ${PRIVATE_MODE} -eq 0 ]; then
-    if pgrep -x "${BROWSER}" > /dev/null; then
-        osascript -e "${ACTIVATE_BROWSER}"  -e "${ACTIVATE_KEYS} \"n\" using command down"
-    else
-        osascript -e "${ACTIVATE_BROWSER}"
-    fi
+if pgrep -x "${BROWSER}" > /dev/null; then
+    osascript -e "${ACTIVATE_BROWSER}"  -e "${ACTIVATE_KEYS} \"n\" using command down"
 else
-    osascript -e "${ACTIVATE_BROWSER}" -e "${ACTIVATE_KEYS} \"${PRIVATE}\" using {command down, shift down}"
+    osascript -e "${ACTIVATE_BROWSER}"
 fi
 
 return ${SUCCESS} 2> /dev/null || exit ${SUCCESS}
