fb39e79e34da421e1a1bdf43b90b14ac1dc7efc1
CrossR <ryan.cross@warwick.ac.uk>
2024-07-15 12:42:16 +0100

    Disable CoPilot for now.

--- neovim/.config/nvim/lua/plugins/copilot.lua
+++ neovim/.config/nvim/lua/plugins/copilot.lua
@@ -4,18 +4,18 @@ return {
         "zbirenbaum/copilot.lua",
         cmd = "Copilot",
         event = "InsertEnter",
-        config = function()
-            require("copilot").setup({
-                panel = {
-                    enabled = true,
-                    auto_refresh = true,
-                },
-                suggestion = {
-                    enabled = true,
-                    auto_trigger = true,
-                    hide_during_completion = false,
-                },
-            })
-        end,
+        -- config = function()
+        --     require("copilot").setup({
+        --         panel = {
+        --             enabled = true,
+        --             auto_refresh = true,
+        --         },
+        --         suggestion = {
+        --             enabled = true,
+        --             auto_trigger = true,
+        --             hide_during_completion = false,
+        --         },
+        --     })
+        -- end,
     },
 }
